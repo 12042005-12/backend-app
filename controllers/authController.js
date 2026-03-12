f90e8dca14a618ecf09aa949fe8ea3c6d141af98
Gayathri <gayathriravinderreddy@gmail.com>
2026-03-12 15:34:24 +0530

    updated

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,7 @@ const validateUser = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (isMatch) {
       req.session.user = user;
-      res.redirect("/https://frontend-app-kohl-iota.vercel.app/store");
+      res.redirect("/store");
     } else {
       res.redirect("/auth/login");
     }
