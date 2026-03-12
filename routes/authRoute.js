badf9002a14f47c366555ccd07451ca17143b915
Gayathri <gayathriravinderreddy@gmail.com>
2026-03-12 15:13:11 +0530

    UPDATED

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,22 +1,11 @@
-const validateUser = async (req, res) => {
-  const { email, password } = req.body;
+import {login,validateUser,register,registerUser,logout} from "../controllers/authController.js"
+import express from "express"
+const authRouter = express.Router()
 
-  const user = await userModel.findOne({ email, role: "admin" });
+authRouter.get("/login",login)
+authRouter.post("/login",validateUser)
+authRouter.get("/register",register)
+authRouter.post("/register",registerUser)
+authRouter.get("/logout",logout)
 
-  // if user not found
-  if (!user) {
-    return res.render("auth/login", { error: "Invalid user" });
-  }
-
-  const isMatch = await bcrypt.compare(password, user.password);
-
-  // if password wrong
-  if (!isMatch) {
-    return res.render("auth/login", { error: "Incorrect password" });
-  }
-
-  // login success
-  req.session.user = user;
-
-  res.redirect("/store");
-};
+export default authRouter
